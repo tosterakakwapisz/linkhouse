505406b5d827c5e9c2aaccb44c9874e7c9b083b7
thetoster <kasper.knapik@gmail.com>
2024-03-23 23:05:42 +0100

    Add route for articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,4 +9,26 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ArticleController extends AbstractController
 {
+    #[Route('/articles', name: 'readArticleCollection', methods: ['GET'])]
+    public function readArticleCollection(
+        FeedService $feedService
+    ): Response {
+        $articles = $feedService->fetchFeedData();
+        if ($articles === null) {
+            return $this->json([
+                'message' => 'There was a problem getting articles',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $mappedArticles = array_map(function (array $article) {
+            return [
+                'guid' => $article['guid'],
+                'title' => $article['title'],
+                'pubDate' => $article['pubDate'],
+                'category' => $article['category'],
+            ];
+        }, $articles);
+
+        return $this->json($mappedArticles, Response::HTTP_OK);
+    }
 }
