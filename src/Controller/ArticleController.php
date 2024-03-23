beced31eac43396edf5acbdc85e5108c99871fb0
thetoster <kasper.knapik@gmail.com>
2024-03-23 23:06:11 +0100

    Add route for reading single article (not finished yet)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -31,4 +31,34 @@ class ArticleController extends AbstractController
 
         return $this->json($mappedArticles, Response::HTTP_OK);
     }
+
+    #[Route('/article/{guid}', name: 'readArticle', methods: ['GET'])]
+    public function readArticle(
+        FeedService $feedService,
+        string $guid
+    ): Response {
+        $articles = $feedService->fetchFeedData();
+        if ($articles === null) {
+            return $this->json([
+                'message' => 'There was a problem getting article',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $key = array_search($guid, array_column($articles, 'guid'));
+        if ($key === false) {
+            return $this->json([
+                'message' => 'Article with guid: '.$guid.' was not found.',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $article = $articles[$key];
+
+        return $this->json([
+            'guid' => $article['guid'],
+            'title' => $article['title'],
+            'link' => $article['link'],
+            'description' => $article['description'],
+            'category' => $article['category'],
+        ], Response::HTTP_OK);
+    }
 }
