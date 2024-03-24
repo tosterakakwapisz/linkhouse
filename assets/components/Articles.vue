57303915365b691ae6fc27a018902d15f66d4f46
tosterakakwapisz <kasper.knapik@gmail.com>
2024-03-24 22:32:17 +0100

    Add articles and home component

---
+++ assets/components/Articles.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar :navItems="navItems" activeRoute="/articles" />
+  </div>
+</template>
+
+<script>
+  import Navigation from "./Navigation.vue";
+
+  export default {
+    name: "Articles",
+    components: {
+      Navbar: Navigation,
+    },
+  };
+</script>
---
+++ assets/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar :navItems="navItems" activeRoute="/" />
+  </div>
+</template>
+
+<script>
+  import Navigation from "./Navigation.vue";
+
+  export default {
+    name: "Home",
+    components: {
+      Navbar: Navigation,
+    },
+  };
+</script>
